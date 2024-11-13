3dae84d0e989cd9fef8992caced6902cb098f176
krishna <krishcsengg@gmail.com>
2024-11-13 04:03:30 +0000

    Update TestFile.java

--- src/TestFile.java
+++ src/TestFile.java
@@ -1,1 +1,10 @@
 TO_CHAR(created_at, 'MM-DD-YYYY HH:MI:SS AM') AS created_at_12hr
+
+
+  <insert id="insertUserActions" parameterType="map">
+        INSERT INTO user_actions (user_id, action_type, file_name)
+        VALUES
+        <foreach collection="fileNames" item="fileName" separator=",">
+            (#{userID}, #{actionType}, #{fileName})
+        </foreach>
+    </insert>
