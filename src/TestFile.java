877b9cab6cd459fa9f7f3466920039e652b60a0d
krishna <krishcsengg@gmail.com>
2024-11-28 02:03:38 +0000

    Update TestFile.java

--- src/TestFile.java
+++ src/TestFile.java
@@ -1,3 +1,1 @@
- I’d like to better understand the scope of the role being offered, as I want to ensure there’s alignment between the responsibilities and expectations for the position.
-
-Based on my experience and expertise in backend technologies, particularly Java, as well as market standards for similar roles in the UK, the package offered appears significantly lower than what is typical for someone with my skill set and background.
+spring.datasource.hikari.initialization-fail-timeout=0
