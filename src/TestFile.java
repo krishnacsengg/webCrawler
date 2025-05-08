b6023580fafc94bd4ba6778633102ad28e0ecf36
krishna <krishcsengg@gmail.com>
2025-05-08 16:24:40 +0100

    Update TestFile.java

--- src/TestFile.java
+++ src/TestFile.java
@@ -1,7 +1,3 @@
-# Install UTF-8 support
-RUN yum install -y glibc-langpack-en
+One of the users has raised a concern regarding a timestamp mismatch between the EDP screen and the File Manager. Currently, EDP shows timestamps in UTC, while the File Manager (built in Angular) displays them in the user's system timezone.
 
-# Set system-wide UTF-8 locale
-ENV LANG en_US.UTF-8
-ENV LANGUAGE en_US:en
-ENV LC_ALL=en_US.UTF-8
+To address this inconsistency, can we create a story to update the time details in the Run & Monitor screens as per the user profile's timezone setting? Additionally, this timezone value can be passed as a parameter to the File Manager, which we will then use to update and display the timestamps accordingly.
